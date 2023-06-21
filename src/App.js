ce2a6c2b69b5e7f94f176667604d229f635bd3e6
wknowpark797 <wknowpark797@gmail.com>
2023-06-21 15:56:09 +0900

    JSX 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
-	return (
-		<div className='App'>
-			<h1>Hello World</h1>
-		</div>
-	);
+	return <h1>Hello World</h1>;
 }
+
+/*
+	function App() {
+		return document.createElement('h1').innerText('Hello World');	
+	}
+
+	- 해당 코드를 JSX로 표현
+	- DOM 요소를 반환하는 컴포넌트
+	- 컴포넌트 함수 이름은 대문자로 시작
+*/
 
 export default App;
