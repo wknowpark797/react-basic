87147e89896fde2fed8fc4b14a0d11dff92196a4
wknowpark797 <wknowpark797@gmail.com>
2023-06-22 11:52:01 +0900

    스타일 적용

---
+++ src/App2.js
@@ -0,0 +1,24 @@
+// sass loader가 함께 들어가 있어 watch 할 필요가 없다.
+import './scss/style.scss';
+
+function App2() {
+	const fontStyle = {
+		color: 'green',
+		fontSize: 60,
+		fontWeight: 'normal',
+	};
+
+	return (
+		<>
+			<div className='wrap'>
+				{/* 정의한 스타일 객체를 변수에 선언 후 가상돔에 연결 */}
+				<h1 style={fontStyle}>Title1</h1>
+
+				{/* 가상돔에 직접 객체 리터럴 형식으로 스타일 적용 */}
+				<h1 style={{ color: 'orange', fontSize: 30 }}>Title2</h1>
+			</div>
+		</>
+	);
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import App2 from './App2';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter>
-			<App />
+			<App2 />
 		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
