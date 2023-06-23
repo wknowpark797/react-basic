b62857e5b27e32943c9965ef47b7dfda451e6857
wknowpark797 <wknowpark797@gmail.com>
2023-06-23 12:57:48 +0900

    google 웹폰트 설정

---
+++ src/App9.js
@@ -0,0 +1,15 @@
+/* Google Font 연결 */
+// import 연결 추천
+
+import './scss/style.scss';
+
+function App9() {
+	return (
+		<>
+			<p className='txt1'>Hello</p>
+			<p className='txt2'>World</p>
+		</>
+	);
+}
+
+export default App9;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import ReactDOM from 'react-dom';
 // import App5 from './App5';
 // import App6 from './App6';
 // import App7 from './App7';
-import App8 from './App8';
+// import App8 from './App8';
+import App9 from './App9';
 
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
 	<HashRouter>
-		<App8 />
+		<App9 />
 	</HashRouter>,
 	document.getElementById('root')
 );
